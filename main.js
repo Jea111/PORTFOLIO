437151523062d1a625f84cdf0bc42a229a853056
jea111 <laliendra42@gmail.com>
2025-08-25 19:13:06 -0500

    Portfolio personal

--- main.js
+++
@@ -1,7 +0,0 @@
-await fetch("http://localhost:3000/api/guardar-pedido", {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({ productos: carrito }),
-});
-//   swal("Pedido realizado", "Gracias por tu compra", "success");
-//   carrito.length = 0;
