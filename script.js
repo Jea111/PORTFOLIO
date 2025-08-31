ca22cc5d16553593da8d5a6a95c47440ccb15ac5
jea111 <laliendra42@gmail.com>
2025-08-31 16:25:22 -0500

    Portafolio personal

---
+++ script.js
@@ -0,0 +1,8 @@
+Toastify({
+  text: "Bienvenido\nPortafolio personal de JEHAN CORTES",
+  duration: 8000,
+  close: true,
+  gravity: "top",
+  position: "right",
+  backgroundColor: "linear-gradient(to right, #00b09b, #6c63ff)",
+}).showToast();
